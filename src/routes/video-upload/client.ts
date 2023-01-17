ee146d67c9d0b1713b31c14aefa419ebc95342f4
ahiipsa <ahiipsa@gmail.com>
2023-01-17 20:46:52 +0400

    Updates client.ts

--- src/routes/video-upload/client.ts
+++ src/routes/video-upload/client.ts
@@ -3,6 +3,45 @@ import {config} from "../../config";
 const HOST = config.uploader.host;
 
 export const client = {
+
+  requestNonce: async (address: string) => {
+    const response = await fetch(`${HOST}/auth/web3/nonce`, {
+      method: 'POST',
+      body: JSON.stringify({address}),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
+
+    const responseData = await response.json();
+
+    return responseData.data;
+  },
+
+  auth: async ({signature, address}: {signature: string, address: string}) => {
+    const response = await fetch(`${HOST}/auth/web3/signature`, {
+      method: 'POST',
+      body: JSON.stringify({signature, address}),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
+
+    const responseData = await response.json();
+
+    return responseData.data;
+  },
+
+  checkJWT: (jwt: string) => {
+    return fetch(`${HOST}/auth/web3/test`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `bearer ${jwt}`
+      },
+    }).then((result) => result.status === 200).catch(() => false);
+  },
+
   loadVideoList: async () => {
     const response = await fetch(`${HOST}/videos`, {
       method: 'Get',
