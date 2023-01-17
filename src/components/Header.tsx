31cc503dcf7fc8566ad278593b026b384adad695
ahiipsa <ahiipsa@gmail.com>
2023-01-17 20:52:18 +0400

    Added web3 auth, updated Header for authed user, and added locking route component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import {Header as GrommetHeader, Nav} from "grommet"
+import React, {useCallback} from 'react';
+import {Header as GrommetHeader, Nav, Avatar, Button} from "grommet"
+import {Logout} from "grommet-icons";
 import {AnchorLink} from "./AnchorLink";
+import {metamaskStore} from "../stores/stores";
 
 interface Props {}
 
 export const Header: React.FC<Props> = () => {
+  const handleLogout = useCallback(() => {
+    metamaskStore.logout();
+  }, []);
+
   return (
     <GrommetHeader background="light-3" pad="small">
+      <Avatar background="brand">0x</Avatar>
       <Nav direction="row">
-        <AnchorLink to="/upload" label="Upload" />
-        <AnchorLink to="/videos" label="List of videos" />
+        <AnchorLink to="/upload" alignSelf="center" label="Upload" />
+        <AnchorLink to="/videos" alignSelf="center" label="List of videos" />
+        <Button hoverIndicator onClick={handleLogout} icon={<Logout />} />
       </Nav>
     </GrommetHeader>
   )
