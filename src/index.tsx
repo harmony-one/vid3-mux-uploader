1697e0deaeaf05546fb05f6d5aedf2635308b118
ahiipsa <ahiipsa@gmail.com>
2023-01-18 15:49:46 +0400

    Deleted unused code

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import "./index.css";
-
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
